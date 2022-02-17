0ee84509aea8f372153e387446931adce06bc095
Liu Wanwei <lion.mathews@gmail.com>
2022-02-18 00:19:18 +0800

    调整菜单，加入 building/index，但没有创建表格和数据

--- appsc/views/unit/menu.php
+++ appsc/views/unit/menu.php
@@ -8,11 +8,19 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div>
-    <p><?= Html::a('从 Excel 导入数据', ['import'], [
+    <p><?= Html::a('从 Excel 导入单位数据', ['unit/import'], [
         'class' => 'btn btn-warning',
         'data' => [
             'confirm' => '确定要重新导入吗?'
         ]
     ]) ?></p>
+
+    <p><?= Html::a('从 Excel 导入建筑数据', ['building/import'], [
+        'class' => 'btn btn-warning',
+        'data' => [
+            'confirm' => '确定要重新导入吗?'
+        ]
+    ]) ?></p>
+
     <p><?= Html::a('清除 memcache 缓存', ['flush-cache'], ['class' => 'btn btn-success']) ?></p>
 </div>
